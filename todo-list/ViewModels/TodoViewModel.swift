3a40e9f4584bb8ea580d36a9561523ec751dcda2
Luiz Mello <contato@luizmello.dev>
2024-10-13 16:34:19 -0300

    Final step 0

--- todo-list/ViewModels/TodoViewModel.swift
+++ todo-list/ViewModels/TodoViewModel.swift
@@ -5,41 +5,40 @@
 //  Created by Luiz Mello on 25/09/24.
 //
 import Foundation
-import Combine
 
 class CategoriesViewModel: ObservableObject {
+    
     @Published var categories: [Category] = []
-    private var cancellables = Set<AnyCancellable>()
     
     private let baseURL = "http://localhost:8000"
     
     func fetchCategories() {
-            guard let url = URL(string: "\(baseURL)/categories_with_todos") else {
-                print("Invalid URL for fetching categories")
+        guard let url = URL(string: "\(baseURL)/categories_with_todos") else {
+            print("Invalid URL")
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            if let error = error {
+                print("Error fetching categories: \(error)")
                 return
             }
             
-            let task = URLSession.shared.dataTask(with: url) { data, response, error in
-                if let error = error {
-                    print("Error fetching categories: \(error)")
-                    return
-                }
-                
-                guard let data = data else {
-                    print("No data received")
-                    return
-                }
-                
-                do {
-                    let categories = try JSONDecoder().decode([Category].self, from: data)
-                    DispatchQueue.main.async {
-                        self.categories = categories
-                    }
-                } catch {
-                    print("Error decoding categories: \(error)")
-                }
+            guard let data = data else {
+                print("No data received")
+                return
             }
             
-            task.resume()
+            do {
+                let decodedCategories = try JSONDecoder().decode([Category].self, from: data)
+                DispatchQueue.main.async {
+                    self?.categories = decodedCategories
+                }
+            } catch {
+                print("Error decoding categories: \(error)")
+            }
         }
+        
+        task.resume()
+    }
 }
