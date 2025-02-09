69e4063f9a70666a36da6d1cc0b255022de909e0
Luiz Mello <contato@luizmello.dev>
2025-02-09 16:59:17 -0300

    add logger

--- todo-list/Networking/NetworkManager.swift
+++ todo-list/Networking/NetworkManager.swift
@@ -24,7 +24,7 @@ class NetworkManager: NetworkManagerProtocol {
         body: Data? = nil
     ) -> AnyPublisher<T, NetworkError> {
         guard let url = URL(string: "\(baseURL)\(endpoint)") else {
-            logError("Invalid URL for endpoint: \(endpoint)")
+            Logger.error("Invalid URL for endpoint: \(endpoint)")
             return Fail(error: NetworkError.invalidURL).eraseToAnyPublisher()
         }
         
@@ -36,59 +36,32 @@ class NetworkManager: NetworkManagerProtocol {
             authentication: authentication,
             body: body
         ) else {
-            logError("Failed to build request for endpoint: \(endpoint)")
+            Logger.error("Failed to build request for endpoint: \(endpoint)")
             return Fail(error: NetworkError.badServerResponse).eraseToAnyPublisher()
         }
         
-        // Log da requisição enviada
-        logInfo("📤 Sending request to: \(url)")
+        Logger.info("📤 Sending request to: \(url)")
         if let body = body, let bodyString = String(data: body, encoding: .utf8) {
-            logInfo("📄 Request body: \(bodyString)")
+            Logger.info("📄 Request body: \(bodyString)")
         }
-        logInfo("🔑 Request headers: \(request.allHTTPHeaderFields ?? [:])")
+        Logger.info("🔑 Request headers: \(request.allHTTPHeaderFields ?? [:])")
         
         return URLSession.shared.dataTaskPublisher(for: request)
             .tryMap { result in
                 guard let response = result.response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
-                    self.logError("⚠️ Error: Bad server response (status code: \(String(describing: (result.response as? HTTPURLResponse)?.statusCode)))")
+                    Logger.error("⚠️ Error: Bad server response (status code: \(String(describing: (result.response as? HTTPURLResponse)?.statusCode)))")
                     throw NetworkError.badServerResponse
                 }
                 return result.data
             }
             .handleEvents(receiveOutput: { data in
-                // Log da resposta recebida
-                if let jsonString = String(data: data, encoding: .utf8) {
-                    self.logInfo("✅ Response received:\n\(jsonString)")
-                    self.logSeparator()
-                }
+                Logger.prettyPrintJSON(from: data)
             })
             .decode(type: T.self, decoder: JSONDecoder())
             .mapError { error in
-                self.logError("❌ Error decoding \(T.self): \(error)")
+                Logger.error("❌ Error decoding \(T.self): \(error)")
                 return error as? NetworkError ?? NetworkError.decodingError
             }
             .eraseToAnyPublisher()
     }
-    
-    // Função para logar mensagens de sucesso
-    private func logInfo(_ message: String) {
-        print("\n\("🌟".green) \(message)\n")
-    }
-    
-    // Função para logar mensagens de erro
-    private func logError(_ message: String) {
-        print("\n\("🚨".red) \(message)\n")
-    }
-    
-    // Função para logar separadores de resposta
-    private func logSeparator() {
-        print("\n\("-----------------------------".yellow)\n")
-    }
-}
-
-extension String {
-    var red: String { return "\u{001B}[0;31m\(self)\u{001B}[0;39m" }
-    var green: String { return "\u{001B}[0;32m\(self)\u{001B}[0;39m" }
-    var yellow: String { return "\u{001B}[0;33m\(self)\u{001B}[0;39m" }
-    var blue: String { return "\u{001B}[0;34m\(self)\u{001B}[0;39m" }
 }
